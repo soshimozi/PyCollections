26cb3aa4690fe26c71b26a7166d588bddda249c2
Scott McCain <scott.mccain@gmail.com>
2012-03-18 21:51:18 -0700

    adding AATree files

---
+++ testtree.py
@@ -0,0 +1,15 @@
+import tree
+
+t = tree.Tree()
+
+t.insert('T')
+print('============')
+t.insert('B')
+print('============')
+t.insert('A')
+print('============')
+t.insert('X')
+print('============')
+t.insert('R')
+print('============')
+
